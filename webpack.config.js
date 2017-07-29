fa9fe6b3b7e8c49b4b4c22c74cf60365caadf161
Daniel Doran <ddoran3@gmail.com>
2017-07-28 22:02:38 -0600

    setup for new stack test

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = {
+	context: path.resolve(__dirname, "src"),
+	entry: "./test.ts",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	resolve: { extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"] },
+	devtool: "source-map",
+	module: { rules: [{ test: /\.tsx?$/, use: "ts-loader" }] },
+	devServer: {
+		host: "0.0.0.0",
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		disableHostCheck: true
+	}
+};
