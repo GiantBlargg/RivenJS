4064cc4e3464b41b75e2c37b83b1b7809c99462e
Daniel Doran <ddoran3@gmail.com>
2017-08-22 17:39:32 -0600

    gulp can now launch the dev-server

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const gulp = require("gulp"),
 	{ exec } = require("child_process"),
 	glob = require("glob"),
-	webpack = require("webpack-stream");
+	webpack = require("webpack"),
+	webpackDevServer = require("webpack-dev-server"),
+	webpackCfg = require("./webpack.config.js");
 
+const wasmGlob = "src/**/*.c";
 gulp.task("wasm", cb => {
-	glob("src/**/*.c", (er, files) => {
+	glob(wasmGlob, (er, files) => {
 		if (er) return cb(er);
 		exec("emcc " + files.join(" ") + " -o dist/wasm.js -s WASM=1", err => {
 			if (err) return cb(err);
@@ -12,13 +15,27 @@ gulp.task("wasm", cb => {
 		});
 	});
 });
-
-gulp.task("pack", () => {
-	return gulp.src("src/test.ts")
-		.pipe(webpack(require('./webpack.config.js'), require("webpack")))
-		.pipe(gulp.dest("dist/"));
+gulp.task("watch-wasm", ["wasm"], () => {
+	gulp.watch(wasmGlob, ["wasm"]);
 });
 
-gulp.task("default", ["pack", "wasm"]);
+gulp.task("webpack", (cb) => {
+	webpack(webpackCfg, (err, stats) => {
+		if (err) return cb(err);
+		console.log(stats.toString({ colors: true }));
+		cb();
+	})
+});
+gulp.task("webpack-dev-server", () => {
+	if (typeof webpackCfg.entry == "string") {
+		webpackCfg.entry = [webpackCfg.entry]
+	}
+	if (webpackCfg.entry instanceof Array) {
+		webpackCfg.entry.push("webpack-dev-server/client?/");
+	}
+	new webpackDevServer(webpack(webpackCfg), webpackCfg.devServer).listen(webpackCfg.devServer.port, webpackCfg.devServer.host);
+});
 
-gulp.task("devServe", ["default"]);
+gulp.task("default", ["webpack", "wasm"]);
+
+gulp.task("serve", ["webpack-dev-server", "watch-wasm"]);
--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,10 @@ module.exports = {
 	},
 	devServer: {
 		host: "0.0.0.0",
+		port: 8080,
 		contentBase: path.join(__dirname, "dist"),
 		compress: true,
-		disableHostCheck: true
-	},
-	node: {
-		fs: "empty"
+		disableHostCheck: true,
+		watchContentBase: true
 	}
 };
